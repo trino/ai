3a304db7fdaf4ff89afc639b395facabfafef21b
trino <minesh@trinoweb.com>
2016-07-26 23:21:47 -0400

    Roy

--- public/index3.php
+++ public/index3.php
@@ -1,172 +1,155 @@
 <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
 <script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
 
-
-
-
-
 <script>
-
-
     var client = algoliasearch("J0FIP6YVFA", '9a0c07ff24eb7715a94de82b5a64dfd6');
     var index = client.initIndex('YourIndexName');
     index.search('something', function(success, hits) {
         console.log(success, hits)
     }, { hitsPerPage: 10, page: 0 });
-
-
-
 </script>
+
 <form method="get">
-    <input type="search" id="search" name="search" size=60 value="<?= $_GET["search"]; ?>">
+    <input type="search" id="search" name="search" size=60 value="<?php if(isset($_GET["search"])){ echo $_GET["search"];} ?>">
     <input type="button" id="startspeech" style="display:none;" value="Click to Speak" onclick="startButton(event);">
     <INPUT TYPE="submit" value="Search">
 </form>
 
 <style>
-body{padding:20px;}
+    body{padding:20px;}
     td,tr{border:0 !important;padding-top:0 !important;padding-bottom:0 !important;margin:0 !important;}
     div{border:1px solid #dadada;}
 </style>
+
 <?php
-
-function firstword($Text)
-{
-    $Space = strpos($Text, " ");
-    if ($Space === false) {
-        return $Text;
+    function firstword($Text) {
+        $Space = strpos($Text, " ");
+        if ($Space === false) {
+            return $Text;
+        }
+        return left($Text, $Space);
     }
-    return left($Text, $Space);
-}
 
-if (!isset($_GET["search"]) || !trim($_GET["search"])) {
-    $_GET["search"] = "";
-} else {
-    include("api.php");
-    $con = connectdb("keywordtest");
+    if (!isset($_GET["search"]) || !trim($_GET["search"])) {
+        $_GET["search"] = "";
+    } else {
+        include("api.php");
+        $con = connectdb("keywordtest");
 
-    $word = $_GET["search"];
-    $words = str_replace(" ", "|", $_GET["search"]);
-    /*
-      $results["SQL"] = "
-      SELECT
-      r.name as restaurant_name,
-      c.name as category_name,
-      i.name as item_name, i.description as description,i.price as price,
-      ca.name as category_addon_name,
-      a.name as addon_name
+        $word = $_GET["search"];
+        $words = str_replace(" ", "|", $_GET["search"]);
+        /*
+          $results["SQL"] = "
+          SELECT
+          r.name as restaurant_name,
+          c.name as category_name,
+          i.name as item_name, i.description as description,i.price as price,
+          ca.name as category_addon_name,
+          a.name as addon_name
 
-      from restaurants r
-      inner join categories c         on r.id = c.restaurant_id
-      inner join items i              on c.id = i.category_id
-      inner join addon_categories ca  on i.id = ca.item_id
-      inner join addons a             on ca.id = a.addon_category_id
+          from restaurants r
+          inner join categories c         on r.id = c.restaurant_id
+          inner join items i              on c.id = i.category_id
+          inner join addon_categories ca  on i.id = ca.item_id
+          inner join addons a             on ca.id = a.addon_category_id
 
-      where r.name REGEXP '$words'
-      or c.name REGEXP '$words'
-      or i.name REGEXP '$words'
-      or ca.name REGEXP '$words'
-      or a.name REGEXP '$words'
-    ";
-*/
-    $results["SQL"] = "SELECT *
- from menu";
+          where r.name REGEXP '$words'
+          or c.name REGEXP '$words'
+          or i.name REGEXP '$words'
+          or ca.name REGEXP '$words'
+          or a.name REGEXP '$words'
+        ";
+    */
+        $results["SQL"] = "SELECT * FROM menu";
 
-    echo "<div class='col-md-4'>";
+        echo "<div class='col-md-4'>";
 
-    print_r($results);
-    echo '<br>------------------------------------<br>';
-    $result = Query($results["SQL"]);
-    $result = Query($results["SQL"]);
-    $i = 0;
+        print_r($results);
+        echo '<br>------------------------------------<br>';
+        $result = Query($results["SQL"]);
+        $result = Query($results["SQL"]);
+        $i = 0;
 
-    if ($result) {
-        $FirstResult = true;
-        $PrevCategory = "";
+        if ($result) {
+            $FirstResult = true;
+            $PrevCategory = "";
 
-        while ($row = mysqli_fetch_array($result)) {
-            if ($FirstResult) {
-                echo '<TABLE class="table table-sm table-responsive"><TR>';
-                $FirstResult = false;
-            }
+            while ($row = mysqli_fetch_array($result)) {
+                if ($FirstResult) {
+                    echo '<TABLE class="table table-sm table-responsive"><TR>';
+                    $FirstResult = false;
+                }
 
-            foreach ($row as $Key => $Value) {
+                foreach ($row as $Key => $Value) {
 
-                if (!is_numeric($Key) && ($Key == "category" || $Key == "item" || $Key == "price" || $Key == "id" || $Key == "toppings" || $Key == "wings_sauce")) {
-                    if ($Key == 'category' && $Value != $PrevCategory) {
-                        echo ' <TD> ' . $Value . ' </TD> ';
-                        $PrevCategory = $Value;
-                    } else if ($Key != 'category') {
-                        echo ' <TD> ';
+                    if (!is_numeric($Key) && ($Key == "category" || $Key == "item" || $Key == "price" || $Key == "id")) {
+                        if ($Key == 'category' && $Value != $PrevCategory) {
+                            echo ' <TD> ' . $Value . ' </TD> ';
+                            $PrevCategory = $Value;
+                        } else if ($Key != 'category') {
+                            echo ' <TD> ';
 
-                        if ($Key == 'item') {
-                            echo '<img src="http://orderpizzaplace.com/SiteFiles/124/Menu/PepperoniPizza350.png"/ width="20"> ';
+                            if ($Key == 'item') {
+                                echo '<img src="http://orderpizzaplace.com/SiteFiles/124/Menu/PepperoniPizza350.png"/ width="20"> ';
+                            }
+
+                            echo $Value . ' </TD>';
+                        } else {
+                            echo ' <TD></TD> ';
                         }
-
-                        echo  $Value . ' </TD>';
-                    } else {
-                        echo ' <TD></TD> ';
                     }
                 }
+                echo '</TR>';
+                $i++;
             }
-            echo '</TR>';
-            $i++;
-        }
-        if (!$FirstResult) {
-            echo '</TABLE>';
-        }
-    } else {
-        echo "No keywords found in '" . $_GET["search"] . "'<P>";
-    }
-
-    echo "</div>";
-
-}
-echo "</div>";
-
-echo "<div class='col-md-4'>";
-
-$results["SQL"] = "SELECT *
- from toppings";
-print_r($results);
-echo '<br>------------------------------------<br>';
-$result = Query($results["SQL"]);
-if ($result) {
-    while ($row = mysqli_fetch_array($result)) {
-        foreach ($row as $Key => $Value) {
-            if (!is_numeric($Key) && ($Key == "topping" || $Key == "id")) {
-                echo $Value . ' ';
+            if (!$FirstResult) {
+                echo '</TABLE>';
             }
+        } else {
+            echo "No keywords found in '" . $_GET["search"] . "'<P>";
         }
-        echo "<br>";
 
-    }
+        echo "</div>";
 
-}
-echo "</div>";
 
-echo "<div class='col-md-4'>";
+        echo "</div>";
 
-$results["SQL"] = "SELECT *
- from wings_sauce";
-print_r($results);
-echo '<br>------------------------------------<br>';
-$result = Query($results["SQL"]);
-$result = Query($results["SQL"]);
-if ($result) {
-    while ($row = mysqli_fetch_array($result)) {
-        foreach ($row as $Key => $Value) {
-            if (!is_numeric($Key) && ($Key == "sauce" || $Key == "id")) {
-                echo $Value . ' ';
+        echo "<div class='col-md-4'>";
+
+        $results["SQL"] = "SELECT * FROM toppings";
+        print_r($results);
+        echo '<br>------------------------------------<br>';
+        $result = Query($results["SQL"]);
+        if ($result) {
+            while ($row = mysqli_fetch_array($result)) {
+                foreach ($row as $Key => $Value) {
+                    if (!is_numeric($Key) && ($Key == "topping" || $Key == "id")) {
+                        echo $Value . ' ';
+                    }
+                }
+                echo "<br>";
             }
         }
-        echo "<br>";
+        echo "</div><div class='col-md-4'>";
+
+        $results["SQL"] = "SELECT * FROM wings_sauce";
+        print_r($results);
+        echo '<br>------------------------------------<br>';
+        $result = Query($results["SQL"]);
+        $result = Query($results["SQL"]);
+        if ($result) {
+            while ($row = mysqli_fetch_array($result)) {
+                foreach ($row as $Key => $Value) {
+                    if (!is_numeric($Key) && ($Key == "sauce" || $Key == "id")) {
+                        echo $Value . ' ';
+                    }
+                }
+                echo "<br>";
+            }
+        }
+
+        echo "</div>";
     }
-
-}
-
-echo "</div>";
-
 ?>
 
 
