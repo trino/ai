46d161054039bd1057f5fc191f8f3e044d97bcd4
trino <minesh@trinoweb.com>
2016-10-05 11:05:41 -0400

    Roy 11:05

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
     <STYLE>
         .bodycontainer{
-            margin-top: 0px !important
+            margin-top: 0px !important;
         }
         .header{
             vertical-align: middle;
@@ -20,6 +20,47 @@
         .header .pull-left{
             margin-left: 15px;
         }
+
+        .dropdown-toggle{
+            display: none;
+        }
+
+        #page {
+            display:block;
+            width:100%;
+            height:95vh !important;
+            overflow:hidden;
+        }
+        #content {
+            float:left;
+            width:100%;
+            height:100%;
+            display:block;
+            overflow:hidden;
+
+            background-color: red;
+        }
+        #content div {
+            height:100%;
+        }
+
+        .v-wrap{
+            height: 100%;
+            white-space: nowrap;
+            text-align: center;
+        }
+        .v-wrap:before{
+            content: "";
+            display: inline-block;
+            vertical-align: middle;
+            height: 100%;
+        }
+        .v-box{
+            display: inline-block;
+            vertical-align: middle;
+            white-space: normal;
+            width:95%;
+        }
     </STYLE>
     <DIV class="header">
         <SPAN CLASS="pull-left">London Pizza</SPAN>
@@ -28,21 +69,35 @@
         <BUTTON CLASS="btn btn-sm btn-primary" onclick="handlelogin('login');">Log In</BUTTON>
         <BUTTON CLASS="btn btn-sm btn-secondary" onclick="handlelogin('forgotpassword');">Forgot Password</BUTTON>
     </DIV>
-    <DIV CLASS="col-md-6">
-        Place holder
-    </DIV>
-    <DIV CLASS="col-md-6">
-        <FORM Name="regform" id="regform">
-            <?= view("popups.edituser"); ?>
-            <DIV STYLE="margin-top: 15px;">
-                <DIV CLASS="col-md-4">
-                    <button class="btn btn-block btn-primary">
-                        Register
-                    </button>
-                </DIV>
+
+    <div id="page">
+        <div id="content">
+            <DIV CLASS="col-md-6">
+                <div class="v-wrap">
+                    <article class="v-box">
+                        Place holder
+                    </article>
+                </div>
             </DIV>
-        </FORM>
-    </DIV>
+            <DIV CLASS="col-md-6">
+                <div class="v-wrap">
+                    <article class="v-box">
+                        <FORM Name="regform" id="regform">
+                            <?= view("popups.edituser"); ?>
+                            <DIV STYLE="margin-top: 15px;">
+                                <DIV CLASS="col-md-4">
+                                    <button class="btn btn-block btn-primary">
+                                        Register
+                                    </button>
+                                </DIV>
+                            </DIV>
+                        </FORM>
+                    </article>
+                </div>
+            </DIV>
+        </div>
+    </div>
+
     <SCRIPT>
         redirectonlogin = true;
     </SCRIPT>
