67783991abc90dc6a9366b2f05b37def8e27d65d
trino <minesh@trinoweb.com>
2016-10-25 10:53:02 -0400

    Roy 10:52

--- resources/views/email_receipt.blade.php
+++ resources/views/email_receipt.blade.php
@@ -1,6 +1,6 @@
 Here is a copy of your order
 <?php
-    $info = first("SELECT * FROM orders WHERE id = " . $orderid);
-    var_dump($info);
+    //$info = first("SELECT * FROM orders WHERE id = " . $orderid);
+    //var_dump($info);
     echo view("popups_receipt", array("orderid" => $orderid))->render();
 ?>
