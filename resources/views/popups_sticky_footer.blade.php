24293d7d0655942c4710447e90ffcd89bb27226e
trino <minesh@trinoweb.com>
2017-01-10 10:14:26 -0500

    Roy 10:14

--- resources/views/popups_sticky_footer.blade.php
+++ resources/views/popups_sticky_footer.blade.php
@@ -1,5 +1,7 @@
-<div class="fixed-action-btn hidden-sm-up" style="bottom: 45px; right: 10px;">
-    <button class="fab bg-danger" onclick="window.scrollTo(0,document.body.scrollHeight);">
-        <strong id="checkout-total"></strong>
-    </button>
-</div>
+@if(read("id"))
+    <div class="fixed-action-btn hidden-sm-up" style="bottom: 45px; right: 10px;">
+        <button class="fab bg-danger" onclick="window.scrollTo(0,document.body.scrollHeight);">
+            <strong id="checkout-total"></strong>
+        </button>
+    </div>
+@endif
