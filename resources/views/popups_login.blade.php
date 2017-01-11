e8898727dcaedd51f9d6239e75beb0cab7729ed5
trino <minesh@trinoweb.com>
2017-01-11 13:32:08 -0500

    Roy 1:30

--- resources/views/popups_login.blade.php
+++ resources/views/popups_login.blade.php
@@ -4,75 +4,73 @@
 
 <!--div class="modal" id="loginmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"  data-keyboard="false" data-backdrop="static"-->
 
-                <div class="row py-1 " STYLE="background: white;">
+<div class="row py-1 " STYLE="background: white;">
 
-                    <DIV CLASS="col-md-12 py-1">
-                        <h3>Precise Pizza Delivery</h3>
-                    </div>
+    <DIV CLASS="col-md-12 py-1">
+        <h3>Precise Pizza Delivery</h3>
+    </div>
 
-                    <DIV CLASS="col-md-4">
+    <DIV CLASS="col-md-4">
 
-                        <!-- Bootstrap CSS -->
-                        <!-- jQuery first, then Bootstrap JS. -->
-                        <!-- Nav tabs -->
+        <!-- Bootstrap CSS -->
+        <!-- jQuery first, then Bootstrap JS. -->
+        <!-- Nav tabs -->
 
-                        <ul class="nav nav-tabs mb-1" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#profile" role="tab" data-toggle="tab" id="logintab" onclick="skiploadingscreen = false;">Login</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#buzz" role="tab" data-toggle="tab" onclick="skiploadingscreen = false;">Signup</a>
-                            </li>
-                        </ul>
+        <ul class="nav nav-tabs mb-1" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" href="#profile" role="tab" data-toggle="tab" id="logintab" onclick="skiploadingscreen = false;">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#buzz" role="tab" data-toggle="tab" id="signuptab" onclick="skiploadingscreen = false;">Signup</a>
+            </li>
+        </ul>
 
-                        <!-- Tab panes -->
-                        <div class="tab-content mt-1">
-                            <div role="tabpanel" class="tab-pane fade in active" id="profile">
-                                <div class="clearfix"></div>
+        <!-- Tab panes -->
+        <div class="tab-content mt-1">
+            <div role="tabpanel" class="tab-pane fade in active" id="profile">
+                <div class="clearfix"></div>
 
-                                <INPUT TYPE="text" id="login_email" placeholder="Email" class="form-control" onkeydown="enterkey(event, '#login_password');">
-                                <div class="clearfix"></div>
+                <INPUT TYPE="text" id="login_email" placeholder="Email" class="form-control" onkeydown="enterkey(event, '#login_password');">
+                <div class="clearfix"></div>
 
-                                <INPUT TYPE="password" id="login_password" placeholder="Password" class="form-control" onkeydown="enterkey(event, 'login');">
+                <INPUT TYPE="password" id="login_password" placeholder="Password" class="form-control" onkeydown="enterkey(event, 'login');">
 
-                                <BUTTON CLASS="btn btn-success pull-right" onclick="handlelogin('login');">Log In</BUTTON>
-                                <BUTTON CLASS="btn btn-secondary pull-left" onclick="handlelogin('forgotpassword');">Forgot Password</BUTTON>
-                            </div>
+                <BUTTON CLASS="btn btn-success pull-right" onclick="handlelogin('login');">Log In</BUTTON>
+                <BUTTON CLASS="btn btn-secondary pull-left" onclick="handlelogin('forgotpassword');">Forgot Password</BUTTON>
+            </div>
 
-                            <div role="tabpanel" class="tab-pane fade" id="buzz">
+            <div role="tabpanel" class="tab-pane fade" id="buzz">
 
-                                <div class="clearfix"></div>
-                                <FORM Name="regform" id="regform">
-                                    <?= view("popups_edituser", array("phone" => false, "autocomplete" => "new-password"))->render(); ?>
-                                </FORM>
-                                <div class="clearfix"></div>
+                <div class="clearfix"></div>
+                <FORM Name="regform" id="regform">
+                    <?= view("popups_edituser", array("phone" => false, "autocomplete" => "new-password"))->render(); ?>
+                </FORM>
+                <div class="clearfix"></div>
 
-                                <FORM id="addform">
-                                    <?php
-                                        if (!read("id")) {
-                                            echo view("popups_address", array("style" => 1))->render();
-                                        }
-                                    ?>
-                                </FORM>
+                <FORM id="addform">
+                    <?php
+                        if (!read("id")) {
+                            echo view("popups_address", array("style" => 1))->render();
+                        }
+                    ?>
+                </FORM>
 
-                                <div class="clearfix"></div>
+                <div class="clearfix"></div>
 
-                                <button class="btn btn-primary pull-right" onclick="register();">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
+                <button class="btn btn-primary pull-right" onclick="register();">
+                    Register
+                </button>
+            </div>
+        </div>
 
-                        <DIV CLASS="clearfix"></DIV>
+        <DIV CLASS="clearfix"></DIV>
 
-                    </DIV>
+    </DIV>
 
-                    <DIV CLASS="col-md-6" id="loginpanel">
-
-                        <DIV CLASS="clearfix"></DIV>
-
-                    </DIV>
-                </div>
+    <DIV CLASS="col-md-6" id="loginpanel">
+        <DIV CLASS="clearfix"></DIV>
+    </DIV>
+</div>
 
 
 
@@ -149,6 +147,10 @@
                 return false;
             }
         });
+    });
+
+    $(document).ready(function () {
+        $("#profile").removeClass("fade").removeClass("in");
     });
 </SCRIPT>
 
